32b264c6eaab7f11dec30758b2eb377c961d2647
Chardymov Oleg <oleg.chardymov@gmail.com>
2020-08-08 11:33:04 +0300

    Add product locators

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,13 @@ class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REG_FORM =  (By.CSS_SELECTOR, "#register_form")
+    REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BASKET_SUCCESS = (By.CSS_SELECTOR, ".alert-success")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_NAME = (By.CSS_SELECTOR, "#messages .alert:first-child .alertinner strong")  #should be first on page
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner p strong")
