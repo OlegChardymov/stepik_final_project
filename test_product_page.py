a190bed352b6e7a84cfe0da7507f67cf7baf9355
Chardymov Oleg <oleg.chardymov@gmail.com>
2020-08-09 16:40:51 +0300

    Add tests
    added 3 negative tests

--- test_product_page.py
+++ test_product_page.py
@@ -23,3 +23,31 @@ def test_guest_can_add_product_to_basket(browser, link):
     browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
     page.solve_quiz_and_get_code()
     page.should_match_product_and_basket()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_product_page()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_product_page()
+    browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_product_page()
+    browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+    page.should_success_basket_msg_disappear()
