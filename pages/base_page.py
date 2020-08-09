f2693530dec18f22001eb28936b72c7dacfa0b3a
Chardymov Oleg <oleg.chardymov@gmail.com>
2020-08-09 16:16:16 +0300

    Add funcs
    is_dissapeared, is_element_not_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, \
+    NoAlertPresentException, TimeoutException
 from math import log, sin
 
 
@@ -12,12 +14,28 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
         return True
+
+    def is_element_not_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).\
+                until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
